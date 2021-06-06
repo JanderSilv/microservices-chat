afc54cfcb17f01b2e97abcd7e08ec58d1b5d7ab6
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2021-06-06 19:07:59 -0300

    feat: setup database connection

---
+++ users-service/src/db/connection.ts
@@ -0,0 +1,18 @@
+import config from 'config';
+import { Connection, createConnection } from 'typeorm';
+
+import User from './entities/User';
+
+let connection: Connection;
+
+export const initConnection = async () => {
+  connection = await createConnection({
+    entities: [User],
+    type: 'mysql',
+    url: config.get('USERS_SERVICE_DB_URL') as string,
+  });
+};
+
+const getConnection = () => connection;
+
+export default getConnection;
